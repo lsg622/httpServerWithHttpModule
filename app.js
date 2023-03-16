e5d929ddf450c37551210b1fc46566ec60370a1b
Lim seong-geun <123074891+lsg622@users.noreply.github.com>
2023-03-16 21:57:34 +0900

    [ADD} 유저 회원가입 엔드포인트 구현

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const http = require("http");
-
+// app.js
+const http = require("http"); // (1)
 const server = http.createServer();
 
 const users = [
+  // (2)
   {
     id: 1,
     name: "Rebekah Johnson",
@@ -12,51 +13,57 @@ const users = [
   {
     id: 2,
     name: "Fabian Predovic",
-    email: "Conne1129@gmail.com",
+    email: "Connell29@gmail.com",
     password: "password",
   },
 ];
-const post = [
+
+const posts = [
   {
     id: 1,
     title: "간단한 HTTP API 개발 시작!",
-    content: "Node.js에 내장되어 있는 http 모듈울 사용해서 HTTP server를 구현",
+    description:
+      "Node.js에 내장되어 있는 http 모듈을 사용해서 HTTP server를 구현.",
     userId: 1,
   },
   {
     id: 2,
     title: "HTTP의 특성",
-    content: "Request/Response와 Stateless!!",
+    description: "Request/Response와 Stateless!!",
     userId: 2,
   },
 ];
 
 const httpRequestListener = function (request, response) {
   const { url, method } = request;
-
   if (method === "GET") {
     if (url === "/ping") {
-      response.written(200, { "Content-Type": "application/json" });
+      response.writeHead(200, { "Content-Type": "application/json" });
       response.end(JSON.stringify({ message: "pong" }));
     }
   } else if (method === "POST") {
-    if (url === "/users/signup") {
-      let body = "";
+    // (3)
+    if (url === "/users") {
+      let body = ""; // (4)
+
       request.on("data", (data) => {
         body += data;
-      });
+      }); // (5)
 
+      // stream을 전부 받아온 이후에 실행
       request.on("end", () => {
-        const user = JSON.parse(body);
+        // (6)
+        const user = JSON.parse(body); //(7)
 
         users.push({
+          // (8)
           id: user.id,
           name: user.name,
           email: user.email,
           password: user.password,
         });
 
-        response.end(JSON.stringify({ message: "userCreated" }));
+        response.end(JSON.stringify({ message: "ok!" })); // (9)
       });
     }
   }
@@ -65,5 +72,5 @@ const httpRequestListener = function (request, response) {
 server.on("request", httpRequestListener);
 
 server.listen(8000, "127.0.0.1", function () {
-  console.log("Listening to request on port 8000");
+  console.log("Listening to requests on port 8000");
 });
